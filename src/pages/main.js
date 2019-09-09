d2b3b441484cb67ddfbe7a01fa108e6a2e5b0c67
romluc <romulofilhofilho@gmail.com>
2019-09-09 10:39:25 -0300

    Add style to the products list

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import api from '../services/api';
 
 // import { Container } from './styles';
@@ -24,10 +30,22 @@ export default class Main extends Component {
     this.setState({ docs });
   };
 
+  renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+
+      <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+        <Text style={styles.productButtonText}>Access</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <FlatList
+          contentContainerStyle={styles.list}
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
@@ -36,3 +54,53 @@ export default class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+
+  list: {
+    padding: 20,
+  },
+
+  productContainer: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#008b8b',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#008b8b',
+    fontWeight: 'bold',
+  },
+});
